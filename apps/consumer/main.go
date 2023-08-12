09c26e39ee7c3df250b9c07d659b24aff25cb9ba
Emerson Valentim <emevalentim0@gmail.com>
2023-08-12 18:52:15 -0300

    feat: add kafka integration

--- apps/consumer/main.go
+++ apps/consumer/main.go
@@ -1,9 +1,34 @@
-package consumer
+package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/IBM/sarama"
 )
 
 func main() {
-	fmt.Print("Consumer")
+	// Create a new Kafka consumer
+	consumer, err := sarama.NewConsumer([]string{"kafka:9092"}, nil)
+	if err != nil {
+		log.Fatal("Error creating Kafka consumer:", err)
+	}
+	defer consumer.Close()
+
+	// Define the Kafka topic
+	topic := "test-topic"
+
+	// Consume messages from the Kafka topic
+	partitionConsumer, err := consumer.ConsumePartition(topic, 0, sarama.OffsetOldest)
+	if err != nil {
+		log.Fatal("Error consuming partition:", err)
+	}
+	defer partitionConsumer.Close()
+
+	for {
+		select {
+		case msg := <-partitionConsumer.Messages():
+			fmt.Printf("Received message: %s\n", string(msg.Value))
+		}
+	}
 }
--- apps/rest/main.go
+++ apps/rest/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
+	"github.com/IBM/sarama"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +14,28 @@ func main() {
 
 	// Define a route
 	router.GET("/", func(c *gin.Context) {
+		producer, err := sarama.NewSyncProducer([]string{"kafka:9092"}, nil)
+
+		if err != nil {
+			log.Fatal("Error creating Kafka producer:", err)
+		}
+		defer producer.Close()
+
+		// Define the Kafka topic and message
+		topic := "test-topic"
+		message := "Hello, Kafka!"
+
+		// Produce the message to the Kafka topic
+		_, _, err = producer.SendMessage(&sarama.ProducerMessage{
+			Topic: topic,
+			Value: sarama.StringEncoder(message),
+		})
+		if err != nil {
+			log.Fatal("Error sending message:", err)
+		}
+
+		fmt.Println("Message sent successfully!")
+
 		c.JSON(200, gin.H{
 			"message": "Hello, Gin!",
 		})
